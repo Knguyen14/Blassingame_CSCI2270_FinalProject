4809eece68a46d3824e23a247da4a4296a666fa8
ross <ross.blassingame@colorado.edu>
2016-04-26 18:38:04 -0600

    added all updated files

--- final/include/class.h
+++ final/include/class.h
@@ -1,14 +1,82 @@
 #ifndef CLASS_H
 #define CLASS_H
 
+#include <string>
+#include <vector>
 
-class class
+struct HashElem{
+	std::string title;
+	int year;
+	HashElem *next;
+    HashElem *previous;
+
+	HashElem(){};
+	HashElem(std::string in_title, int in_year)
+	{
+		title = in_title;
+		year = in_year;
+		next = NULL;
+		previous = NULL;
+	}
+
+};
+
+struct MovieNode{
+    int ranking;
+    std::string title;
+    int year;
+    int quantity;
+    MovieNode *parent;
+    MovieNode *leftChild;
+    MovieNode *rightChild;
+
+    MovieNode(){};
+
+    MovieNode(int in_ranking, std::string in_title, int in_year, int in_quantity)
+    {
+        ranking = in_ranking;
+        title = in_title;
+        year = in_year;
+        quantity = in_quantity;
+    }
+
+};
+
+class HashTable
 {
-    public:
-        class();
-        virtual ~class();
-    protected:
-    private:
+	public:
+		HashTable();
+		~HashTable();
+		void initializeTable();
+		void printTableContents();
+		void insertMovie(std::string name, int year);
+		void deletehashMovie(std::string name);
+		void findhashMovie(std::string name);
+
+
+
+		void printMovieInventory();
+        void addMovieNode(int ranking, std::string title, int releaseYear, int quantity);
+        void findMovie(std::string title);
+        void rentMovie(std::string title);
+        void postOrder();
+        void countMovies();
+        void deleteMovie(std::string title);
+        MovieNode *treeMin(MovieNode *node);
+	private:
+		int hashSum(std::string x, int s);
+		int tableSize = 10;
+		HashElem* hashTable[10];
+
+
+
+		void printMovieInventory(MovieNode * node);
+        MovieNode* search(std::string title);
+        MovieNode *root;
+        void postOrder(MovieNode *node);
+        void countMovies(MovieNode *node);
+        int movieCounter;
+
 };
 
 #endif // CLASS_H
--- final/main.cpp
+++ final/main.cpp
@@ -1,9 +1,106 @@
 #include <iostream>
-
-using namespace std;
+#include "class.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    HashTable table;
+    HashTable tree;
+
+    table.initializeTable();
+    int menu_choice;
+    std::cout << "======Main Menu======" << std::endl;
+    std::cout << "1. Insert movie into hash table" << std::endl;
+    std::cout << "2. Delete movie from hash table" << std::endl;
+    std::cout << "3. Find movie in hash table" << std::endl;
+    std::cout << "4. Print hash table contents" << std::endl;
+    std::cout << "5. Add a movie to tree" << std::endl;
+    std::cout << "6. Find a movie in tree" << std::endl;
+    std::cout << "7. Rent a movie in tree" << std::endl;
+    std::cout << "8. Print the inventory from tree" << std::endl;
+    std::cout << "9. Delete a movie from tree" << std::endl;
+    std::cout << "10. Count the movies in tree" << std::endl;
+    std::cout << "11. Quit" << std::endl;
+    std::cin >> menu_choice;
+    while (menu_choice != 11) {
+        if (menu_choice == 1) {
+            std::string title1;
+            int year;
+            std::cout << "Enter title:" << std::endl;
+            std::cin.ignore();
+            getline(std::cin, title1);
+            std::cout << "Enter year:" << std::endl;
+            std::cin >> year;
+            table.insertMovie(title1, year);
+        }
+        else if (menu_choice == 2) {
+            std::string delete_title;
+            std::cout << "Enter title:" << std::endl;
+            std::cin.ignore();
+            getline(std::cin, delete_title);
+            table.deletehashMovie(delete_title);
+        }
+        else if (menu_choice == 3) {
+            std::string find_title;
+            std::cout << "Enter title:" << std::endl;
+            std::cin.ignore();
+            getline(std::cin, find_title);
+            table.findhashMovie(find_title);
+        }
+        else if (menu_choice == 4) {
+            table.printTableContents();
+        }
+        else if (menu_choice == 5) {
+            std::string tree_title;
+            int tree_year;
+            std::cout << "title: " << std::endl;
+            std::cin.ignore();
+            getline(std::cin, tree_title);
+            std::cout << "year: " << std::endl;
+            std::cin >> tree_year;
+            tree.addMovieNode(1, tree_title, tree_year, 1);
+        }
+        else if (menu_choice == 6) {
+            std::cout << "Enter title:" << std::endl;
+            std::string user_title_find;
+            std::cin.ignore();
+            getline(std::cin, user_title_find);
+            tree.findMovie(user_title_find);
+        }
+        else if (menu_choice == 7) {
+            std::cout << "Enter title:" << std::endl;
+            std::string user_title_rent;
+            std::cin.ignore();
+            getline(std::cin, user_title_rent);
+            tree.rentMovie(user_title_rent);
+        }
+        else if (menu_choice == 8) {
+            tree.printMovieInventory();
+        }
+        else if (menu_choice == 9) {
+            std::cout << "Enter title:" << std::endl;
+            std::string user_title_delete;
+            std::cin.ignore();
+            getline(std::cin, user_title_delete);
+            tree.deleteMovie(user_title_delete);
+
+        }
+        else if (menu_choice == 10) {
+            tree.countMovies();
+        }
+        std::cout << "======Main Menu======" << std::endl;
+        std::cout << "1. Insert movie into hash table" << std::endl;
+        std::cout << "2. Delete movie from hash table" << std::endl;
+        std::cout << "3. Find movie in hash table" << std::endl;
+        std::cout << "4. Print hash table contents" << std::endl;
+        std::cout << "5. Add a movie to tree" << std::endl;
+        std::cout << "6. Find a movie in tree" << std::endl;
+        std::cout << "7. Rent a movie in tree" << std::endl;
+        std::cout << "8. Print the inventory from tree" << std::endl;
+        std::cout << "9. Delete a movie from tree" << std::endl;
+        std::cout << "10. Count the movies in tree" << std::endl;
+        std::cout << "11. Quit" << std::endl;
+        std::cin >> menu_choice;
+    }
+    std::cout << "Goodbye!" << std::endl;
+    tree.postOrder();
 }
